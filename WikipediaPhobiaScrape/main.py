88c4f5fecda9d251e892a78d1d8d1414caad8720
Alex Ocegueda <alexocegueda59@gmail.com>
2023-07-08 02:03:08 -0500

    Web scraper for wikipedia phobias

---
+++ WikipediaPhobiaScrape/main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+
+# Make a request to the Wikipedia page
+url = 'https://en.wikipedia.org/wiki/List_of_phobias'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+trs = soup.find_all('tr')
+data = []
+
+# Iterate over the <tr> tags and store their content 
+for tr in trs:
+    # Extract the first word before the word "fear"
+    fear_index = tr.text.find('fear')
+    if fear_index != -1:
+        first_word = tr.text[:fear_index].strip()
+    else:
+        continue
+
+    # Remove newline characters, \u, and \n from the rest of the content
+    rest_of_content = re.sub(r'\n+', ' ', tr.text[fear_index+4:].strip())
+    cleaned_content = re.sub(r'\\[un]', '', rest_of_content)
+
+    # Remove brackets with numbers and any word matching "/dislikes"
+    cleaned_content = re.sub(r'\[[0-9]+\]', '', cleaned_content)
+
+    full_content = f"fear {cleaned_content}"
+
+    # Create a dictionary for each <tr> tag
+    tr_dict = {"name": first_word, "description": full_content}
+    data.append(tr_dict)
+
+# Save the data as a JSON file
+with open('scraped_data.json', 'w') as f:
+    json.dump(data, f)
